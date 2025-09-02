034ae392100352da4aa8ad5136473f63470dff90
Owen Deines <143202010+OwenTheJayhawk@users.noreply.github.com>
2025-09-01 22:29:36 -0500

    Add files via upload

---
+++ Board.py
@@ -0,0 +1,28 @@
+from Tile import Tile
+import random
+from collections import deque
+
+class Board:
+    def __init__(self, width = 10, height = 10, m_count = 10):
+        x = 1
+
+    def board_reset(self, m_count = None):
+        x = 1
+
+    def _find_adj_mines(self, K, U):
+        K = 1
+
+    def set_mines(self, start): #start should be safe
+        x = 1
+
+    def show_tile(self, K, U):
+        x = 1
+
+    def anime_show(self):
+        x = 1
+
+    def flag_on_off(self, K, U):
+        K = 1
+
+    def win_check(self):
+        x = 1
