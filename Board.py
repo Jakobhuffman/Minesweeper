68bc42bab92586e28d6f80f63872ca15d7ca3b5c
Owen Deines <143202010+OwenTheJayhawk@users.noreply.github.com>
2025-09-03 21:49:25 -0500

    Update Board.py

--- Board.py
+++ Board.py
@@ -57,13 +57,76 @@ class Board:
             for j, k in self._find_adj_mines(a, n):
                 self.make_grid[j][k].adj_mines += 1
     def show_tile(self, K, U):
-        x = 1
+        if self.fail_state or self.win_state:
+            return None
+        space = self.make_grid[K][U]
+        if not self.are_mines_placed:
+            self.set_mines((K, U))
+            self.are_mines_placed = True
+        if space.flagged or space.is_revealed or space.full_reveal:
+            return None
+        if space.is_mine:
+            self.mine_explosion = (K, U)
+            self.fail_state = True
+            space.is_revealed = True
+            for r in self.make_grid:
+                for c in r:
+                    if c.is_mine:
+                        c.is_revealed = True
+
+            return 'game over'
+        
+        space.is_revealed = True
+        if space.adj_mines == 0:
+            for j, k in self._find_adj_mines(K, U):
+                adj = self.make_grid[k][j]
+                if (not adj.is_revealed) and (not adj.flagged) and (not adj.full_reveal):
+                    if not adj.is_mine:
+                        adj.full_reveal = True
+                        self.board_reveal.append((j, k))
+                        self.is_reveal_done = True
+
+        if self.win_check():
+            return 'Win!'
+        return None
 
     def anime_show(self):
-        x = 1
+        if not self.board_reveal:
+            return
+        K, U = self.board_reveal.popleft()
+        space = self.make_grid[U][K]
+        space.is_revealed = True
+        space.full_reveal = False
+        if space.adj_mines == 0:
+            for k, u in self._find_adj_mines(K, U):
+                adj = self.make_grid[u][k]
+                if (not adj.is_revealed) and (not adj.flagged) and (not adj.full_reveal):
+                    if not adj.is_mine:
+                        adj.full_reveal = True
+                        self.board_reveal.append((k, u))
+        if not self.board_reveal:
+            self.is_reveal_done = False
+            self.win_check()
 
     def flag_on_off(self, K, U):
-        K = 1
-
+        if self.fail_state or self.win_state:
+            return
+        space = self.make_grid[U][K]
+        if space.is_revealed:
+            return
+        if space.flagged:
+            space.flagged = False
+            self.flags_placed -= 1
+        else:
+            if self.flags_placed < self.mine_count:
+                space.flagged = True
+                self.flags_placed += 1
     def win_check(self):
-        x = 1
+        if self.fail_state:
+            return False
+        for r in self.make_grid:
+            for c in r:
+                if not c.is_mine and not c.is_revealed:
+                    return False
+        self.win_state = True
+        return True
