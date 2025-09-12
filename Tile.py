e15afef66a6821fc2370bbd5780bd54d6b547adc
Owen Deines <143202010+OwenTheJayhawk@users.noreply.github.com>
2025-09-12 02:40:56 -0500

    comment Tile.py

--- Tile.py
+++ Tile.py
@@ -1,14 +1,14 @@
 class Tile:
     def __init__(self):
-        self.is_mine = False
-        self.is_revealed = False
-        self.flagged = False
-        self.adj_mines = 0
-        self.full_reveal = False
+        self.is_mine = False #Shows if the tile is a mine
+        self.is_revealed = False #Shows if the status of the tile is revealed to the player or not
+        self.flagged = False #Shows if the tile has been flagged by the player
+        self.adj_mines = 0 #Shows number of adjacent mines to the tile
+        self.full_reveal = False #Shows if the tile has been revealed in the full board reveal
 
     def reset_tile(self):
-        self.is_mine = False
-        self.is_revealed = False
-        self.flagged = False
-        self.adj_mines = 0
-        self.full_reveal = False
+        self.is_mine = False #Resets mine/not mine status
+        self.is_revealed = False #Hides tile
+        self.flagged = False #unflags tile
+        self.adj_mines = 0 #Sets adjacent mine count to 0
+        self.full_reveal = False #Resets full board reveal
